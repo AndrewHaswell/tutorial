11a2d6c6a6869e8e93f96a0bba64c6cf89bb451b
Andrew Haswell <andrew.haswell@visualsoft.co.uk>
2015-05-16 12:03:46 +0100

    Lesson 5 Blade Fundamentals

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,21 @@
+@extends('app')
+
+@section('content')
+
+    <h1>About</h1>
+
+    @if (count($people))
+        <h3>People I like:</h3>
+
+        <ul>
+            @foreach ($people as $person)
+                <li>{{$person}}</li>
+            @endforeach
+        </ul>
+    @endif
+
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad animi consequatur delectus dolore ducimus fugiat
+        hic molestiae molestias, nemo nihil nobis quam quidem sapiente sint tempore ullam veniam, voluptas voluptatibus?
+    </p>
+@stop
